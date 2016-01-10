e64ffec32bfbcab6dded28e083e7cdca87ced89c
axellegupta <aguptaad@connect.ust.hk>
2016-01-10 22:21:56 +0800

    NavigationDrawer

--- TravelBuddy/app/src/main/java/com/example/axelle/travelbuddy/NavigationDrawerFragment.java
+++ TravelBuddy/app/src/main/java/com/example/axelle/travelbuddy/NavigationDrawerFragment.java
@@ -105,6 +105,7 @@ public class NavigationDrawerFragment extends Fragment {
                         getString(R.string.title_section1),
                         getString(R.string.title_section2),
                         getString(R.string.title_section3),
+                        getString(R.string.title_section4),
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
@@ -248,7 +249,7 @@ public class NavigationDrawerFragment extends Fragment {
         }
 
         if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Welcome to TravelBuddy.", Toast.LENGTH_SHORT).show();
             return true;
         }
 
