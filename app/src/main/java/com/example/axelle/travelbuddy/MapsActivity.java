b26522c536c61875b3e25bd9cb719090f609b688
axellegupta <aguptaad@connect.ust.hk>
2016-01-13 01:46:39 +0800

    map actionbar midway

--- app/src/main/java/com/example/axelle/travelbuddy/MapsActivity.java
+++ app/src/main/java/com/example/axelle/travelbuddy/MapsActivity.java
@@ -1,17 +1,22 @@
 package com.example.axelle.travelbuddy;
 
-        import com.google.android.gms.maps.*;
-        import com.google.android.gms.maps.model.*;
-        import android.app.Activity;
-        import android.os.Bundle;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MapsActivity extends Activity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
+      //  Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+       //  setSupportActionBar(myToolbar);
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -19,14 +24,22 @@ public class MapsActivity extends Activity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-33.867, 151.206);
+        // LatLng hk = new LatLng(22.2783, 114.1747);
 
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+        //map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(22.2783, 114.1747), 16));
 
+        // You can customize the marker image using images bundled with
+        // your app, or dynamically generated bitmaps.
         map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.common_plus_signin_btn_icon_dark_disabled))
+                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                .position(new LatLng(0, 0)));
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
