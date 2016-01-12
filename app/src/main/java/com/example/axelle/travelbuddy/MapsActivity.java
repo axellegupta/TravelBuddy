aa7631dfd5be57a6733a8739e02dc3988c814bf2
axellegupta <aguptaad@connect.ust.hk>
2016-01-13 02:41:42 +0800

    map actionbar halfway

--- app/src/main/java/com/example/axelle/travelbuddy/MapsActivity.java
+++ app/src/main/java/com/example/axelle/travelbuddy/MapsActivity.java
@@ -4,10 +4,13 @@ import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -32,14 +35,36 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // You can customize the marker image using images bundled with
         // your app, or dynamically generated bitmaps.
-        map.addMarker(new MarkerOptions()
+        final LatLng A = new LatLng(22.2783, 114.1747);
+        Marker a = map.addMarker(new MarkerOptions()
+                .position(A)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.common_plus_signin_btn_icon_dark_disabled))
+                .draggable(true)
+                .title("START")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        final LatLng B = new LatLng(22.2780, 114.1740);
+        Marker b = map.addMarker(new MarkerOptions()
+                .position(B)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.common_plus_signin_btn_icon_dark_disabled))
+                .draggable(true)
+                .title("END")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+       /* map.addMarker(new MarkerOptions()
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.common_plus_signin_btn_icon_dark_disabled))
                 .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
-                .position(new LatLng(0, 0)));
+                .position(new LatLng(0, 0)));*/
     }
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
